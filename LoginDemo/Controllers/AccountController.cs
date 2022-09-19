35938537c066babda756405906aa79ce2e786483
KothaiVani <kothainagaraj3@gmail.com>
2022-09-19 18:31:37 +0530

    Add project files.

---
+++ LoginDemo/Controllers/AccountController.cs
@@ -0,0 +1,90 @@
+﻿using LoginDemo.Models;
+using LoginDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Security;
+
+namespace LoginDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        Repository repository = new Repository();
+        // GET: Account
+        public ActionResult Index()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(LoginViewModel model) //Login
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+
+            var result = await repository.login(model);
+            if (result.resultCode == 200)
+            {                
+                return RedirectToAction("Index", "Home"); 
+            }
+            else
+            {
+                ModelState.AddModelError("", result.message);
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Register() //Register
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Logout() 
+        {
+            SignOut();
+
+            return RedirectToAction("Index"); 
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Register(LoginViewModel model) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+
+            var result = await repository.Register(model);
+
+            if (result.resultCode == 200)
+            {
+                return RedirectToAction("Index"); //redirect to login form
+            }
+            else
+            {
+                ModelState.AddModelError("", result.message);
+            }
+
+            return View();
+        }
+               
+    }
+}
---
+++ LoginDemo/Service/Repository.cs
@@ -0,0 +1,101 @@
+﻿using LoginDemo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LoginDemo.Services
+{
+    public class Repository
+    {
+        string connectionstring = "Data Source=DESKTOP-NGQAKEI;Initial Catalog=SampleWorkouts;Persist Security Info=True;User ID=kothai;Password=abc123";      
+        public async Task<ResponseModel<string>> login(LoginViewModel user)
+        {
+            ResponseModel<string> response = new ResponseModel<string>();
+
+            if (user != null)
+            {
+                string md5_password = md5_string(user.Password);
+
+                using (SqlConnection conn = new SqlConnection(connectionstring))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = string.Format("Select * FROM tbl_Users WHERE password = '{0}' and Email='{1}'", user.Password, user.Email);
+                    cmd.Connection = conn;
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    await Task.Run(() => da.Fill(dt));
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        response.Data = JsonConvert.SerializeObject(dt);
+                        response.resultCode = 200;
+                    }
+                    else
+                    {
+                        response.message = "User Not Found!";
+                        response.resultCode = 500;
+                    }
+
+
+                }
+            }
+            return response;
+        }
+
+        public async Task<ResponseModel<string>> Register(LoginViewModel user)
+        {
+            ResponseModel<string> response = new ResponseModel<string>();
+
+            if (user != null)
+            {
+                string md5_password = md5_string(user.Password);
+
+
+
+                using (SqlConnection conn = new SqlConnection(connectionstring))
+                {
+                    string date = "2020-10-11";
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = string.Format("Insert INTO tbl_Users(Email,Password,Reg_Date) VALUES('{0}','{1}','{2}')", user.Email, user.Password, date);
+                    cmd.Connection = conn;
+
+                    conn.Open();
+                    var result = await cmd.ExecuteNonQueryAsync();
+                    conn.Close();
+                    if (result == 1) //row changes in the database - successfull
+                    {
+                        response.message = "User has been registered!";
+                        response.resultCode = 200;
+                    }
+                    else
+                    {
+                        response.message = "Unable to register User!";
+                        response.resultCode = 500;
+                    }
+
+                }
+            }
+            return response;
+        }
+
+        public string md5_string(string password)
+        {
+            string md5_password = string.Empty;
+            using (MD5 hash = MD5.Create())
+            {
+                md5_password = string.Join("", hash.ComputeHash(Encoding.UTF8.GetBytes(password)).Select(x => x.ToString("x2")));
+            }
+
+            return md5_password;
+        }
+    }
+}
